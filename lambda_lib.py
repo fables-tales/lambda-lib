6c1596241f92fd21531bbb9bf1ccb029ba47d9f1
Sam Phippen <samphippen@googlemail.com>
2011-06-17 22:04:49 +0100

    added some more higher level functions to lambda lib
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lambda_lib.py
+++ lambda_lib.py
@@ -17,6 +17,7 @@ def handle_opponent():
     except:
         sys.exit(0)
 
+
 def do_turn(type, card, slot):
     if card in cards:
         if type == LEFT:
@@ -31,3 +32,38 @@ def do_turn(type, card, slot):
     else:
         sys.stderr.write("attempted to play a card that doesn't exist\n")
         sys.exit(255)
+    return handle_opponent()
+
+def reset_to_identity(location):
+    do_turn(LEFT, "put", location)
+
+def accumulate(value, location):
+    do_turn(RIGHT, "zero", location)
+    i = 1
+    do_turn(LEFT,  "succ", location)
+
+    while (i * 2 <= value):
+        do_turn(LEFT, "dbl", location)
+        i *= 2
+
+    while (i + 1 <= value):
+        do_turn(LEFT, "succ", location)
+        i += 1
+
+def apply_to_slot_0(slot):
+    do_turn(LEFT,  "K"     , slot)
+    do_turn(LEFT,  "S"     , slot)
+    do_turn(RIGHT, "get"   , slot)
+    do_turn(RIGHT, "zero"  , slot)
+
+def do_attack(amount, my_slot, their_slot):
+    reset_to_identity(0)
+    accumulate(my_slot, 0)
+    do_turn(RIGHT, "attack", 1)
+    apply_to_slot_0(1)
+    reset_to_identity(0)
+    accumulate(their_slot, 0)
+    apply_to_slot_0(1)
+    reset_to_identity(0)
+    accumulate(amount, 0)
+    apply_to_slot_0(1)
